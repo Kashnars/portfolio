b323c446df5c472d2d8081d6de64475e81254103
Lapprenti <bad_shika_87@hotmail.fr>
2015-09-19 15:42:15 +0200

    a

--- header.php
+++ header.php
@@ -27,6 +27,8 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
 <?php wp_head(); ?>
 </head>
 
---
+++ pages/cv.php
@@ -0,0 +1,90 @@
+<a href="http://localhost/portfolio/wp-content/uploads/2015/09/vb_cv2.pdf" target="_blank">Télécharger au format PDF <span class="glyphicon glyphicon-file" aria-hidden="true"></span></a>
+
+<div class="row" id="cv-wrapper">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
+            <h2>VINCENT BLANCHETON</h2>
+            </div>
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6">
+            <p>3 rue Pierre Ragot 87100 Limoges<br>
+            06 77 71 56 54<br>
+            vincent.blancheton@gmail.com
+            Permis B</p>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h3 class="cv-title"><span class="plus">+</span>Compétences</h3>
+                <h4>Programmation web:</h4>
+                <div class="p-30">
+                    <p>
+                        PHP: Zend, Symfony2<br>
+                        JavaScript: jQuery, AJAX<br>
+                        BDD: MySQL, Oracle<br>
+                        HTML5/CSS3, DOM, XML<br>
+                        Flash: AS3<br>
+                        Ruby on Rails
+                    </p>
+                </div>
+                <h4>Mobile:</h4>
+                <div class="p-30">
+                    <p>
+                        Androïd: Java, Androïd SDK<br>
+                        iOS: Objective-C, Cocoa<br>
+                        Objets connectés: Arduino
+                    </p>
+                </div>
+                <h4>Langues:</h4>
+                <div class="p-30">
+                    <p>
+                        Anglais (Lu, écrit, parlé)<br>
+                        TOEIC: 805/900<br>
+                        Espagnol (Lu, écrit, parlé)<br>
+                        Italien (Notions)
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h3 class="cv-title"><span class="plus">+</span>Diplômes</h3>
+                <p>
+                    2012: Licence Pro Communication et Médias, Développement web de systèmes d'information, Université de cergy-Pontoise (95)<br>
+                    2011: DUT Services et Réseaux de Communication, IUT du Limousin (87)<br>
+                    2009: Bac scientifique, Lycée Saint-Jean (87)
+                </p>
+
+                <h3 class="cv-title"><span class="plus">+</span>expériences</h3>
+                <p>
+                    Sept. 2015 - Aujourd'hui: Développeur web junior, DynAdmic (87)<br>
+                    Nov. 2011 - sept. 2012: Concepteur-Développeur, La Poste DSIC Innovation, Saint-Quentin (78)<br>
+                    Avr. 2011 - Juil. 2011: Développeur stagiaire, 3w-Online, Paris (75)
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h3 class="cv-title"><span class="plus">+</span>Jeux vidéos</h3>
+                <p>
+                    Travail d'équipe<br>
+                    Ecoute<br>
+                    Respect des règles
+                </p>
+            </div>
+            <div class="col-md-6">
+                <h3 class="cv-title"><span class="plus">+</span>Passions</h3>
+                <p>
+                    Musique: Guitare, Basse<br>
+                    High Tech, Actualité mondiale<br>
+                    Association: Trésorier de l'Association de la Communication et des Outils Multimédia (ACOM) 2009-2011
+                </p>
+            </div>
+            <div class="col-md-12">
+                <h3 class="cv-title"><span class="plus">=</span>Concepteur développeur d'applications web et mobiles</h3>
+            </div>
+            <p>
+                J’ai de l’ambition - Le Web m’apporte, je veux apporter au Web<br>
+                Nous avons un avenir ensemble
+            </p>
+        </div>
+    </div>
+</div>
