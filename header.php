416a811caaf47756adc3d24284c6f0529efcf53a
Lapprenti <bad_shika_87@hotmail.fr>
2015-09-19 13:05:24 +0200

    Adding jquery CDN

--- header.php
+++ header.php
@@ -17,6 +17,8 @@
 <!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.min.js"></script>
 <![endif]-->
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
