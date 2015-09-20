2f1d7d8bd5ae89182494f11195431c9b6beb1890
Lapprenti <bad_shika_87@hotmail.fr>
2015-09-20 13:33:32 +0200

    Adding Contact, Presentation and Showroom pages

---
+++ contact.php
@@ -0,0 +1,5 @@
+<div class="row" id="cv-wrapper">
+    <div class="col-md-8 col-md-offset-2">
+        <p style="text-align: center;">[contact-form-7 id="50" title="Formulaire de contact"]</p>
+    </div>
+</div>
---
+++ presentation.php
@@ -0,0 +1,5 @@
+<div class="row" id="cv-wrapper">
+    <div class="col-md-8 col-md-offset-2">
+        A venir.
+    </div>
+</div>
