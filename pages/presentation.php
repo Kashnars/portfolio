ada25603be07961a4d64e1efbfcfdf8dcb02eeee
Lapprenti <bad_shika_87@hotmail.fr>
2015-09-22 21:38:42 +0200

    Creating presentation page

---
+++ pages/presentation.php
@@ -0,0 +1,14 @@
+<div class="row" id="cv-wrapper">
+    <div class="col-md-8 col-md-offset-2">
+        <p>    Je fais partie de la première génération qui a connu une enfance sans les mondes numériques d'aujourd’hui, mais la première à s'être engouffré dedans. C'est après un BAC Scientifique que j'ai découvert plus en détail les métiers de l'internet durant ma formation en Services et Réseaux de Communication. Naturellement doué pour la programmation, j'ai décidé d'approfondir le sujet lors de ma Licence Professionnelle Communication et Médias.<br><br>
+
+    Fan de musique et de jeux vidéos, j’affûte actuellement mes compétences auprès de DynAdmic, afin de devenir à terme un Developer Hero: PHP et Frameworks, JavaScript et jQuery, HTML5, CSS3 et Bootstrap n’auront bientôt plus de secret pour moi.<br><br>
+
+    Découvrez plus en détail mon cursus via mon curriculum vitae, et jetez un coup d’oeil à mon savoir-faire et à mes différents projets personnels dans la section Showroom.<br><br><br>
+
+
+Cheers,<br><br>
+
+    Vincent Blancheton</p>
+    </div>
+</div>
