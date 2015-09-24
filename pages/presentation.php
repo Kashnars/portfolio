575bbca2e77128d4bbdd520b12df8300c9549757
Lapprenti <bad_shika_87@hotmail.fr>
2015-09-24 19:19:18 +0200

    a

--- pages/presentation.php
+++ pages/presentation.php
@@ -1,6 +1,6 @@
 <div class="row" id="cv-wrapper">
     <div class="col-md-8 col-md-offset-2">
-        <p>    Je fais partie de la première génération qui a connu une enfance sans les mondes numériques d'aujourd’hui, mais la première à s'être engouffré dedans. C'est après un BAC Scientifique que j'ai découvert plus en détail les métiers de l'internet durant ma formation en Services et Réseaux de Communication. Naturellement doué pour la programmation, j'ai décidé d'approfondir le sujet lors de ma Licence Professionnelle Communication et Médias.<br><br>
+        <p>    Je fais partie de la première génération qui a connu une enfance sans les mondes numériques d'aujourd’hui, mais la première à s'être engouffrée dedans. C'est après un BAC Scientifique que j'ai découvert plus en détail les métiers de l'internet durant ma formation en Services et Réseaux de Communication. Naturellement doué pour la programmation, j'ai décidé d'approfondir le sujet lors de ma Licence Professionnelle Communication et Médias.<br><br>
 
     Fan de musique et de jeux vidéos, j’affûte actuellement mes compétences auprès de DynAdmic, afin de devenir à terme un Developer Hero: PHP et Frameworks, JavaScript et jQuery, HTML5, CSS3 et Bootstrap n’auront bientôt plus de secret pour moi.<br><br>
 
